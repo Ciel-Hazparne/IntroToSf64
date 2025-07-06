ea30e509a6aecba0628be682ad1324b32ca91b61
Ciel-Hazparne <ciel@ent-st-joseph-hasparren.fr>
2025-07-06 11:41:17 +0200

    CRUD Article

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleForm;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/article')]
+final class ArticleController extends AbstractController
+{
+    #[Route(name: 'article_index', methods: ['GET'])]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        $articles = $articleRepository->findAll();
+        return $this->render('article/index.html.twig', [
+            'current_menu' => 'articles',
+            'articles' => $articles,
+        ]);
+    }
+
+    #[Route('/new', name: 'article_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ArticleRepository $articleRepository): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleForm::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $articleRepository->save($article, true);
+
+            return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('article/new.html.twig', [
+            'current_menu' => 'articles',
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+
+    #[Route('/{id}', name: 'article_show', methods: ['GET'])]
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', [
+            'current_menu' => 'articles',
+            'article' => $article,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'article_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Article $article, ArticleRepository $articleRepository): Response
+    {
+        $form = $this->createForm(ArticleForm::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $articleRepository->save($article, true);
+
+            return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'current_menu' => 'articles',
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+
+    #[Route('/{id}', name: 'article_delete', methods: ['POST'])]
+    public function delete(Request $request, Article $article, ArticleRepository $articleRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->getPayload()->getString('_token'))) {
+            $articleRepository->remove($article, true);
+        }
+
+        return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+}
---
+++ src/Repository/ArticleRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Article;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Article>
+ */
+class ArticleRepository extends ServiceEntityRepository
+{
+
+
+    private EntityManagerInterface $em;
+    public function __construct(ManagerRegistry $registry, EntityManagerInterface $em)
+    {
+        parent::__construct($registry, Article::class);
+        $this->em = $em;
+    }
+
+    public function save(Article $entity, bool $flush = false): void
+    {
+        $this->em->persist($entity);
+
+        if ($flush) {
+            $this->em->flush();
+        }
+    }
+
+    public function remove(Article $entity, bool $flush = false): void
+    {
+        $this->em->remove($entity);
+
+        if ($flush) {
+            $this->em->flush();
+        }
+    }
+
+
+}
