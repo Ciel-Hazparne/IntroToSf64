b4f0bf8033d61ae3444849d588193467e45f38a3
Ciel-Hazparne <ciel@ent-st-joseph-hasparren.fr>
2025-07-07 19:31:42 +0200

    CRUD User + login + inscription

---
+++ src/Form/UserType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Entrez votre adresse mail',
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'required' => true,
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre prénom',
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'required' => true,
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom',
+                ],
+            ])
+            ->add('password',PasswordType::class, [
+                'required' => true,
+                'label' => 'Mot de passe',
+                'attr' => [
+                    'placeholder' => 'Entrez votre mot de passe',
+                ],
+            ])
+            ->add('confirm_password',PasswordType::class, [
+                'required' => true,
+
+                'label' => 'Confirmation du mot de passe',
+                'attr' => [
+                    'placeholder' => 'Entrez à nouveau votre mot de passe',
+                ],
+            ])
+            ->add('roles',ChoiceType::class,[
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Editeur' => 'ROLE_EDITOR',
+                    'Administrateur' => 'ROLE_ADMIN'
+                ],
+                'expanded' => true,
+                'multiple' => true,
+                'label' => 'Rôles'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
